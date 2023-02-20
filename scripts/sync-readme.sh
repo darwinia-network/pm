faa268a14473954fd0e764abdc1a22233e9c60b3
fewensa <37804932+fewensa@users.noreply.github.com>
2023-02-20 12:41:02 +0800

    Fix #1 (#2)
    * Try sync readme
    
    * test
    
    * CI
    
    * Update action name
    
    * Upate action
    
    ---------
    
    Co-authored-by: root <root@vmi539575.contaboserver.net>

---
+++ scripts/sync-readme.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+WORK_PATH=${BIN_PATH}/../
+
+README_FILE=${WORK_PATH}/README.md
+
+prepare_readme() {
+ echo '# Darwinia Project Management Repository' >> ${README_FILE}
+ echo '' >> ${README_FILE}
+}
+
+sync_dev_call() {
+  cd ${WORK_PATH}/notion-backup
+  DEV_CALL_DIRNAME=$(ls -d */ | cut -f1 -d'/' | grep Dev)
+  cd "$DEV_CALL_DIRNAME"
+
+  echo '## Core Dev Meetings' >> ${README_FILE}
+  echo '' >> ${README_FILE}
+  
+  for ENTRY in *.md
+  do
+    DATE=$(echo $ENTRY | cut -d ' ' -f1)
+    echo "- [${DATE} Darwinia Dev Call](notion-backup/${DEV_CALL_DIRNAME}/${ENTRY})" >> ${README_FILE}
+  done
+}
+
+
+main() {
+  > ${README_FILE}
+
+  prepare_readme
+  sync_dev_call
+}
+
+main
