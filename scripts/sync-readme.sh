8c7a50d65d559a1c86ecfef4105c4e1889b5286f
fewensa <37804932+fewensa@users.noreply.github.com>
2023-02-20 13:01:33 +0800

    Sort list

--- scripts/sync-readme.sh
+++ scripts/sync-readme.sh
@@ -20,12 +20,15 @@ sync_dev_call() {
   echo '## Core Dev Meetings' >> ${README_FILE}
   echo '' >> ${README_FILE}
   
+  TMP_FILE=${BIN_PATH}/dev-call.tmp.txt
   for ENTRY in *.md
   do
     DATE=$(echo $ENTRY | cut -d ' ' -f1)
     URL=$(echo notion-backup/${DEV_CALL_DIRNAME}/${ENTRY} | sed -e 's/ /%20/g')
-    echo "- [${DATE} Darwinia Dev Call](${URL})" >> ${README_FILE}
+    MEET_LIN="- [${DATE} Darwinia Dev Call](${URL})"
+    sed -i "1i${MEET_LIN}" ${TMP_FILE}
   done
+  cat ${TMP_FILE} >> ${README_FILE}
 }
 
 
